b071315a55e55874e5f38bd08c3224ffba12ecdb
Daniel Nouri <daniel.nouri@gmail.com>
2014-10-11 19:14:42 +0200

    Add initial version for sklearn wrapper for nntools.

---
+++ nolearn/nntools.py
@@ -0,0 +1,253 @@
+from __future__ import absolute_import
+
+import itertools
+
+from nntools.layers import get_all_layers
+from nntools.layers import get_all_params
+from nntools.updates import nesterov_momentum
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+import theano
+from theano import tensor as T
+
+
+class _list(list):
+    pass
+
+
+class _dict(dict):
+    def __contains__(self, key):
+        return True
+
+
+def negative_log_likelihood(output, prediction):
+    return -T.mean(T.log(output)[T.arange(prediction.shape[0]), prediction])
+
+
+class BatchIterator(object):
+    def __init__(self, batch_size):
+        self.batch_size = batch_size
+
+    def __call__(self, X, y=None, test=False):
+        self.X, self.y = X, y
+        self.test = test
+        return self
+
+    def __iter__(self):
+        n_samples = self.X.shape[0]
+        bs = self.batch_size
+        for i in range((n_samples + bs - 1) / bs):
+            Xb = self.X[i * bs:(i + 1) * bs]
+            if self.y is not None:
+                yb = self.y[i * bs:(i + 1) * bs]
+            else:
+                yb = None
+            yield Xb, yb
+
+
+class NeuralNet(BaseEstimator):
+    def __init__(
+        self,
+        layers,
+        update=nesterov_momentum,
+        loss=negative_log_likelihood,
+        batch_iterator=BatchIterator(batch_size=100),
+        max_epochs=100,
+        eval_size=0.1,
+        on_epoch_finished=None,
+        on_training_finished=None,
+        verbose=0,
+        more_params=None,
+        **kwargs
+        ):
+        self.layers = layers
+        self.update = update
+        self.loss = loss
+        self.batch_iterator = batch_iterator
+        self.max_epochs = max_epochs
+        self.eval_size = eval_size
+        self.on_epoch_finished = on_epoch_finished
+        self.on_training_finished = on_training_finished
+        self.verbose = verbose
+        self.more_params = more_params or {}
+
+        for key in kwargs.keys():
+            assert not hasattr(self, key)
+        vars(self).update(kwargs)
+        self._kwarg_keys = kwargs.keys()
+
+    def fit(self, X, y):
+        self.enc_ = LabelEncoder()
+        y = self.enc_.fit_transform(y).astype(np.int32)
+        self.classes_ = self.enc_.classes_
+
+        out = self.output_layer_ = self._initialize_layers(self.layers)
+
+        iter_funcs = self._create_iter_funcs(out, self.loss, self.update)
+        self.train_iter_, self.eval_iter_, self.predict_iter_ = iter_funcs
+
+        self.train_loop(X, y)
+        return self
+
+    def train_loop(self, X, y):
+        X_train, X_valid, y_train, y_valid = self.train_test_split(
+            X, y, self.eval_size)
+
+        epoch = 0
+        info = None
+
+        while epoch < self.max_epochs:
+            epoch += 1
+
+            train_losses = []
+            valid_losses = []
+            valid_accuracies = []
+
+            for Xb, yb in self.batch_iterator(X_train, y_train):
+                batch_train_loss = self.train_iter_(Xb, yb)
+                train_losses.append(batch_train_loss)
+
+            for Xb, yb in self.batch_iterator(X_valid, y_valid, test=True):
+                batch_valid_loss, accuracy = self.eval_iter_(Xb, yb)
+                valid_losses.append(batch_valid_loss)
+                valid_accuracies.append(accuracy)
+
+            avg_train_loss = np.mean(train_losses)
+            avg_valid_loss = np.mean(valid_losses)
+            avg_valid_accuracy = np.mean(valid_accuracies)
+
+            if self.verbose:
+                print("Epoch %d of %d" % (epoch, self.max_epochs))
+                print("  training loss:\t\t%.6f" % avg_train_loss)
+                print("  validation loss:\t\t%.6f" % avg_valid_loss)
+                print("  validation accuracy:\t\t%.2f %%" %
+                      (avg_valid_accuracy * 100))
+
+            info = dict(
+                epoch=epoch,
+                train_loss=avg_train_loss,
+                valid_loss=avg_valid_loss,
+                valid_accuracy=avg_valid_accuracy,
+                )
+            if self.on_epoch_finished is not None:
+                try:
+                    self.on_epoch_finished(self, info)
+                except StopIteration:
+                    break
+
+        if self.on_training_finished is not None:
+            self.on_training_finished(self, info)
+
+    def predict_proba(self, X):
+        probas = []
+        for Xb, yb in self.batch_iterator(X, test=True):
+            probas.append(self.predict_iter_(Xb))
+        return np.vstack(probas)
+
+    def predict(self, X):
+        y_pred = np.argmax(self.predict_proba(X), axis=1)
+        return self.enc_.inverse_transform(y_pred)
+
+    def score(self, X, y):
+        assert X.dtype == theano.config.floatX
+        return accuracy_score(self.predict(X), y)
+
+    def train_test_split(self, X, y, eval_size):
+        skf = StratifiedKFold(y, 1. / eval_size)
+        train_indices, valid_indices = iter(skf).next()
+        X_train, y_train = X[train_indices], y[train_indices]
+        X_valid, y_valid = X[valid_indices], y[valid_indices]
+        return X_train, X_valid, y_train, y_valid
+
+    def get_all_layers(self):
+        return get_all_layers(self.output_layer_)[::-1]
+
+    def get_all_params(self):
+        return get_all_params(self.output_layer_)[::-1]
+
+    def _create_iter_funcs(self, output_layer, loss_func, update):
+        X = T.matrix('x')
+        y = T.ivector('y')
+        X_batch = T.matrix('x_batch')
+        y_batch = T.ivector('y_batch')
+
+        loss_train = loss_func(
+            output_layer.get_output(X_batch), y_batch)
+        loss_eval = loss_func(
+            output_layer.get_output(X_batch, deterministic=True), y_batch)
+        predict_proba = output_layer.get_output(X_batch, deterministic=True)
+        predict = predict_proba.argmax(axis=1)
+        accuracy = T.mean(T.eq(predict, y_batch))
+
+        all_params = get_all_params(output_layer)
+        update_params = self._get_params_for('update')
+        updates = update(loss_train, all_params, **update_params)
+
+        train_iter = theano.function(
+            inputs=[theano.Param(X_batch), theano.Param(y_batch)],
+            outputs=[loss_train],
+            updates=updates,
+            givens={
+                X: X_batch,
+                y: y_batch,
+                },
+            )
+        eval_iter = theano.function(
+            inputs=[theano.Param(X_batch), theano.Param(y_batch)],
+            outputs=[loss_eval, accuracy],
+            givens={
+                X: X_batch,
+                y: y_batch,
+                },
+            )
+        predict_iter = theano.function(
+            inputs=[theano.Param(X_batch)],
+            outputs=predict_proba,
+            givens={
+                X: X_batch,
+                },
+            )
+
+        return train_iter, eval_iter, predict_iter
+
+    def _get_params_for(self, name):
+        collected = {}
+        prefix = '{}_'.format(name)
+
+        params = vars(self)
+        more_params = self.more_params
+
+        for key, value in itertools.chain(params.items(), more_params.items()):
+            if key.startswith(prefix):
+                collected[key[len(prefix):]] = value
+
+        return collected
+
+    def _initialize_layers(self, layers):
+        input_layer_name, input_layer_factory = layers[0]
+        input_layer_params = self._get_params_for(input_layer_name)
+        layer = input_layer_factory(**input_layer_params)
+
+        for (layer_name, layer_factory) in layers[1:]:
+            layer_params = self._get_params_for(layer_name)
+            layer = layer_factory(layer, **layer_params)
+
+        return layer
+
+    def get_params(self, deep=True):
+        params = super(NeuralNet, self).get_params(deep=deep)
+
+        # Incidentally, nntools layers have a 'get_params' too, which
+        # for sklearn's 'clone' means it would treat it in a special
+        # way when cloning.  Wrapping the list of layers in a custom
+        # list type does the trick here, but of course it's crazy:
+        params['layers'] = _list(params['layers'])
+        return _dict(params)
+
+    def _get_param_names(self):
+        # This allows us to have **kwargs in __init__ (woot!):
+        param_names = super(NeuralNet, self)._get_param_names()
+        return param_names + self._kwarg_keys
---
+++ nolearn/tests/test_nntools.py
@@ -0,0 +1,156 @@
+from mock import patch
+from nntools.layers import DenseLayer
+from nntools.layers import DropoutLayer
+from nntools.layers import InputLayer
+from nntools.nonlinearities import softmax
+from nntools.updates import nesterov_momentum
+import numpy as np
+import pytest
+from sklearn.base import clone
+from sklearn.datasets import fetch_mldata
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.utils import shuffle
+
+
+@pytest.fixture(scope='session')
+def mnist():
+    dataset = fetch_mldata('mnist-original')
+    X, y = dataset.data, dataset.target
+    X = X.astype(np.float32) / 255.0
+    return shuffle(X, y, random_state=42)
+
+
+def test_nntools_functional_mnist(mnist):
+    # Run a full example on the mnist dataset
+    from nolearn.nntools import NeuralNet
+
+    X, y = mnist
+    X_train, y_train = X[:60000], y[:60000]
+    X_test, y_test = X[60000:], y[60000:]
+
+    epochs = []
+
+    def on_epoch_finished(nn, epoch):
+        epochs.append(epoch)
+        if len(epochs) > 1:
+            raise StopIteration()
+
+    nn = NeuralNet(
+        layers=[
+            ('input', InputLayer),
+            ('hidden1', DenseLayer),
+            ('dropout1', DropoutLayer),
+            ('hidden2', DenseLayer),
+            ('dropout2', DropoutLayer),
+            ('output', DenseLayer),
+            ],
+        input_shape=(100, 784),
+        output_num_units=10,
+        output_nonlinearity=softmax,
+
+        more_params=dict(
+            hidden1_num_units=512,
+            hidden2_num_units=512,
+            ),
+
+        update=nesterov_momentum,
+        update_learning_rate=0.01,
+        update_momentum=0.9,
+
+        max_epochs=5,
+        on_epoch_finished=on_epoch_finished,
+        )
+
+    nn.fit(X_train, y_train)
+    assert len(epochs) == 2
+    assert epochs[0]['valid_accuracy'] > 0.85
+    assert epochs[1]['valid_accuracy'] > epochs[0]['valid_accuracy']
+    assert sorted(epochs[0].keys()) == [
+        'epoch', 'train_loss', 'valid_accuracy', 'valid_loss',
+        ]
+
+    y_pred = nn.predict(X_test)
+    assert accuracy_score(y_pred, y_test) > 0.85
+
+
+def test_nntools_functional_grid_search(mnist, monkeypatch):
+    # Make sure that we can satisfy the grid search interface.
+    from nolearn.nntools import NeuralNet
+
+    nn = NeuralNet(
+        layers=[],
+        update=nesterov_momentum,
+        )
+
+    param_grid = {
+        'more_params': [{'hidden_num_units': 100}, {'hidden_num_units': 200}],
+        'update_momentum': [0.9, 0.98],
+        }
+    X, y = mnist
+
+    vars_hist = []
+
+    def fit(self, X, y):
+        vars_hist.append(vars(self).copy())
+        return self
+
+    with patch.object(NeuralNet, 'fit', autospec=True) as mock_fit:
+        mock_fit.side_effect = fit
+        with patch('nolearn.nntools.NeuralNet.score') as score:
+            score.return_value = 0.3
+            gs = GridSearchCV(nn, param_grid, cv=2, refit=False, verbose=4)
+            gs.fit(X, y)
+
+    assert [entry['update_momentum'] for entry in vars_hist] == [
+        0.9, 0.9, 0.98, 0.98] * 2
+    assert [entry['more_params'] for entry in vars_hist] == (
+        [{'hidden_num_units': 100}] * 4 +
+        [{'hidden_num_units': 200}] * 4
+        )
+
+
+def test_clone():
+    from nolearn.nntools import NeuralNet
+    from nolearn.nntools import negative_log_likelihood
+    from nolearn.nntools import BatchIterator
+
+    params = dict(
+        layers=[
+            ('input', InputLayer),
+            ('hidden', DenseLayer),
+            ('output', DenseLayer),
+            ],
+        input_shape=(100, 784),
+        output_num_units=10,
+        output_nonlinearity=softmax,
+
+        more_params={
+            'hidden_num_units': 100,
+            },
+        update=nesterov_momentum,
+        update_learning_rate=0.01,
+        update_momentum=0.9,
+
+        loss=negative_log_likelihood,
+        batch_iterator=BatchIterator(batch_size=100),
+        on_epoch_finished=None,
+        on_training_finished=None,
+        max_epochs=100,
+        eval_size=0.1,
+        verbose=0,
+        )
+    nn = NeuralNet(**params)
+
+    nn2 = clone(nn)
+    params1 = nn.get_params()
+    params2 = nn2.get_params()
+
+    params.pop('batch_iterator')
+    params1.pop('batch_iterator')
+    params2.pop('batch_iterator')
+    params.pop('output_nonlinearity')
+    params1.pop('output_nonlinearity')
+    params2.pop('output_nonlinearity')
+
+    assert params == params1 == params2
